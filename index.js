fae00f68211cdba79642ebfc508d655bba4b654e
makotot <makoto.ttn@gmail.com>
2015-10-18 23:32:12 +0900

    + rules

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  rules: {
+    'string-quotes': [1, 'single'],
+    'color-no-invalid-hex': [2],
+    'number-max-precision': [2, 2],
+    'number-zero-length-no-unit': [2],
+    'block-no-empty': [2],
+    'selector-no-id': [2],
+    'rule-no-single-line': [2]
+  }
+};
