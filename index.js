acbcc475fbc22b127f330b9d37fade176f1bccef
makotot <makoto.ttn@gmail.com>
2016-01-09 18:54:17 +0900

    update rules

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 module.exports = {
   rules: {
-    'string-quotes': [1, 'single'],
-    'color-hex-case': [1, 'upper'],
-    'color-no-invalid-hex': [2],
-    'color-no-named': [2],
-    'number-max-precision': [2, 2],
-    'number-no-trailing-zeros': [2],
-    'number-zero-length-no-unit': [2],
-    'block-no-empty': [2],
-    'selector-no-id': [2],
-    'selector-no-universal': [1],
-    'rule-no-single-line': [2],
-    'rule-single-line-max-declarations': [2, 1]
+    'string-quotes': "single",
+    'color-hex-case': "lower",
+    'color-no-invalid-hex': true,
+    'number-no-trailing-zeros': true,
+    'number-zero-length-no-unit': true,
+    'block-no-empty': true
   }
 };
